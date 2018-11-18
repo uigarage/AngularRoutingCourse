f857729b43ca0bdf436a3565d1b2ec507b0dcab1
Srini <42915005+codewithsrini@users.noreply.github.com>
2018-11-18 22:04:59 +0530

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashComponent } from './dash/dash.component';
+import { LoginComponent } from './auth/login/login.component';
+import { UsersComponent } from './users/users.component';
+import { FourZeroFourComponent } from './four-zero-four/four-zero-four.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'users',
+    component: UsersComponent
+  },
+  {
+    path: 'dash',
+    component: DashComponent
+  },
+  { 
+    path: '', 
+    redirectTo: '/dash',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: FourZeroFourComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashComponent } from './dash/dash.component';
+import { UsersComponent } from './users/users.component';
+import { LoginComponent } from './auth/login/login.component';
+import { FourZeroFourComponent } from './four-zero-four/four-zero-four.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashComponent,
+    UsersComponent,
+    LoginComponent,
+    FourZeroFourComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
